098d89d20b847eba5c6aade51e33ed3b558479a8
italobfa <italobfa@hotmail.com>
2021-10-12 15:59:51 -0300

    funcionalidade tabela

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { ClientTable } from './components/ClientTable';
 import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global'
 
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
       <Header />
+      <ClientTable />
       <GlobalStyle />
     </>
   );
--- src/components/ClientTable/index.tsx
+++ src/components/ClientTable/index.tsx
@@ -2,8 +2,24 @@ import { useState, useEffect } from "react";
 import { Table, Thead, Tbody, Tr, Th, Td } from "react-super-responsive-table";
 import "react-super-responsive-table/dist/SuperResponsiveTableStyle.css";
 
+interface Client {
+  id?: number;
+  name: string;
+  company: string;
+  sector: string;
+  email: string;
+  number: number;
+  site: string;
+}
+
 export function ClientTable() {
-  const [clients, setClients] = useState([]);
+  const [clients, setClients] = useState<Client[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/clients")
+      .then((response) => response.json())
+      .then((data) => setClients(data));
+  }, []);
 
   return (
     <Table>
@@ -18,14 +34,22 @@ export function ClientTable() {
         </Tr>
       </Thead>
       <Tbody>
-        <Tr>
-          <Td></Td>
-          <Td></Td>
-          <Td></Td>
-          <Td></Td>
-          <Td></Td>
-          <Td></Td>
-        </Tr>
+        {clients.map((client) => {
+          return (
+            <Tr key={client.id}>
+              <Td>{client.name}</Td>
+              <Td>{client.company}</Td>
+              <Td>{client.sector}</Td>
+              <Td>{client.number}</Td>
+              <Td>{client.email}</Td>
+              <Td>
+                <a href={client.site} target="_blank" rel="noreferrer noopener">
+                  Acessar Site
+                </a>
+              </Td>
+            </Tr>
+          );
+        })}
       </Tbody>
     </Table>
   );
